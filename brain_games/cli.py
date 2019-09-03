3b19c11ffdb61331a7d92e06b19f361997e53426
mari <zefiratakuet@yandex.ru>
2019-09-03 21:38:27 +0300

    Added Makefile, prompt

---
+++ brain_games/cli.py
@@ -0,0 +1,4 @@
+def run():
+    import prompt
+    name = prompt.string('May I have your name? ')
+    print("Hello, {}!".format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-
 def main():
     print('Welcome to the Brain Games!')
+    import brain_games.cli
+    brain_games.cli.run()
+
 
 if __name__ == '__main__':
     main()
