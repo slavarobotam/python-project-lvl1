4889204b1a0aaea79545c5561933b18b404838bb
mari <zefiratakuet@yandex.ru>
2019-09-16 21:13:28 +0300

    lint fixed 3

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,9 +1,1 @@
-from . import brain_even, brain_gcd, brain_prime, brain_progression, brain_calc
-
-__all__ = [
-    'brain_even'
-    'brain_gcd'
-    'brain_prime'
-    'brain_progression'
-    'brain_calc'
-    ]
+from . import brain_even, brain_gcd, brain_prime, brain_progression, brain_calc  # noqa
