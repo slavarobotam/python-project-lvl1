86d712565991fa23ac0aa76f7330acc552386790
mari <zefiratakuet@yandex.ru>
2019-09-20 19:27:55 +0300

    lint fixed

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,1 +1,1 @@
-from . import brain_even, brain_gcd, brain_prime, brain_progression, brain_calc  # noqa: F401
+from . import brain_even, brain_gcd, brain_prime, brain_progression, brain_calc  # noqa: F401,E501
--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,5 +1,4 @@
 import random
-import functools
 
 
 def show_description():
